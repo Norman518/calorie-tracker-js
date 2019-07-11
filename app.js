947a0ad63259a0db781ffcf5dfcad65707b7be21
Vincent Teas <vincentteas@gmail.com>
2019-07-10 20:40:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -23,23 +23,19 @@ const StorageCtrl = (() => {
       }
     },
     getItemsFromStorage: () => {
-      let items;
       if (localStorage.getItem("items") === null) {
-        items = [];
-      } else {
-        items = JSON.parse(localStorage.getItem("items"));
+        return [];
       }
-      return items;
+      return JSON.parse(localStorage.getItem("items"));
     },
     updateItemStorage: updatedItem => {
-      let items = JSON.parse(localStorage.getItem("items"));
-
+      let items = JSON.parse(localStorage.getItem('items'));
       items.forEach((item, index)=> {
-        if (updatedItem.id === item.id) {
+        if(updatedItem.id === item.id){
           items.splice(index, 1, updatedItem);
         }
       });
-      localStorage.setItem("items", JSON.stringify(items));
+      localStorage.setItem('items', JSON.stringify(items));
     },
     deleteItemFromStorage: id=> {
       let items = JSON.parse(localStorage.getItem("items"));
